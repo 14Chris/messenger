0c1e769397798b786d30c223bb57e3aa499345d9
Christopher Lenfant <lenfant.chris@hotmail.fr>
2020-11-27 19:12:33 +0100

    Add account activation page

---
+++ messenger-web/src/components/User/ActivateAccount.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      <div v-if="isActivate == null">
+          Activating...
+      </div>
+      <div v-else-if="isActivate == false">
+          An error occured while activating your account. Retry again.
+      </div>
+      <div v-else>
+          Your account have been activated
+      </div>
+  </div>
+</template>
+
+<script>
+// import ApiService from "@/service/api";
+
+// var api = new ApiService();
+export default {
+    data(){
+        return {
+            isActivate:null,
+            token:""
+        }
+    },
+    mounted(){
+        this.token = this.$route.params.token
+        this.ActivateAccount()
+    },
+    methods:{
+        ActivateAccount(){
+            //TODO
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
