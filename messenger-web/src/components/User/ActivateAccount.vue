fc9237c07abf16b580b5bcf282a3d302abe17c11
14Chris <lenfant.chris@hotmail.fr>
2020-11-30 13:08:58 +0100

    Add spinner and api call in activation page

--- messenger-web/src/components/User/ActivateAccount.vue
+++ messenger-web/src/components/User/ActivateAccount.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
       <div v-if="isActivate == null">
-          Activating...
+                <div class="loader-wrapper">
+            <div class="loader is-loading"></div>
+        </div>
       </div>
       <div v-else-if="isActivate == false">
           An error occured while activating your account. Retry again.
       </div>
       <div v-else>
-          Your account have been activated
+          Contrgatulations, your account havs been activated !
       </div>
   </div>
 </template>
 
 <script>
-// import ApiService from "@/service/api";
+import ApiService from "@/service/api";
 
-// var api = new ApiService();
+var api = new ApiService();
 export default {
     data(){
         return {
@@ -29,12 +31,46 @@ export default {
     },
     methods:{
         ActivateAccount(){
-            //TODO
-        }
+            api.getData("users/account_activation/"+this.token)
+            .then((response) => response.json())
+            .then((resp) => {
+              if(resp.ResponseType == 1){
+                  this.isActivate = true
+              }
+              else{
+                  this.isActivate = false
+              }
+          })
+        .catch(() => {
+           this.isActivate = false
+        })
     }
+        }
 }
 </script>
 
-<style>
+<style scoped>
+    .loader-wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background: #fff;
+    opacity: 0;
+    z-index: -1;
+    transition: opacity .3s;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 6px;
+        opacity: 1;
+      z-index: 1;
 
+}
+
+.loader {
+            height: 100px;
+            width: 100px;
+        }
 </style>
