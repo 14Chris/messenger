343af4851e90e8a02a5dd34cdada2b0e42594d1c
14Chris <lenfant.chris@hotmail.fr>
2020-12-16 14:26:46 +0100

    Update MessageController to use ResponseObject and handle HttpCode API results

--- backend/Messenger/Messenger.Api/Controllers/ConversationController.cs
+++ backend/Messenger/Messenger.Api/Controllers/ConversationController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
