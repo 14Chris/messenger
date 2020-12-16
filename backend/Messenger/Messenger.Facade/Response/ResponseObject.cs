f4c7d193f6bfbf8483a45ca9d5c07bde7b3d52b7
14Chris <lenfant.chris@hotmail.fr>
2020-12-16 13:35:23 +0100

    Add ResponseObject class to return data from services methods

---
+++ backend/Messenger/Messenger.Facade/Response/ResponseObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Messenger.Facade.Response
+{
+    public class ResponseObject
+    {
+        public ResponseType ResponseType { get; set; }
+
+        public string Message { get; set; }
+
+        public dynamic Result { get; set; }
+
+
+        public ResponseObject(ResponseType responseType, string message, dynamic result)
+        {
+            this.ResponseType = responseType;
+            this.Message = message;
+            this.Result = result;
+        }
+
+        public ResponseObject(ResponseType responseType)
+        {
+            this.ResponseType = responseType;
+        }
+
+    }
+}
